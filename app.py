d9a72738d026b7896c8c5261de303b03c492d8e8
HARI JOHN <109884650+Harry-kj@users.noreply.github.com>
2025-05-27 21:09:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import json
 import random
+import os
 
 app = Flask(__name__)
 
@@ -20,5 +21,6 @@ def get_verse(emotion):
     verse = random.choice(verses)
     return jsonify(verse)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
